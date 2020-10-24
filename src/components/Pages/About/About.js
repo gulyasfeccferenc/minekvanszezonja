a019327df8e141d7b45f80e5ae3ed556114054bf
Ferenc Gulyás <ferenc.gulyas@itron.com>
2020-10-24 22:44:26 +0200

    Extending About section

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -10,6 +10,8 @@ export default class About extends Component {
                 <Title>Minek van szezonja?</Title>
                 <p>Az oldal tanulóprojektként jött létre, hogy a React keretrendszert minél behatóbban ismerhessem meg egy igazi projekt kapcsán.</p>
                 <p>A fő cél egy olyan könnyen használható webes alkalmazás elkészítése volt, ami kereshető és átlátható formában mutatja be a hazai növények szezonalitását.</p>
+                <h3>0.1</h3>
+                <p>Az oldal jelenleg egy táblázatos nézettel és egy példaadatbázis növényeinek adataival felkerült a <a href="https://minekvanszezonja.hu">https://minekvanszezonja.hu</a> domainre</p>
             </Fragment>
             )
     }
