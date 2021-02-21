65445be6682c1ab470789b92f532a97d9cc00435
gulyasfeccferenc <gulyasfeccferenc@gmail.com>
2021-02-21 22:02:53 +0100

    Making single plant view more nice and working, adding emojis to month constants

--- src/components/Pages/Dashboard/HighlightedPlant/HighlightedPlant.js
+++ src/components/Pages/Dashboard/HighlightedPlant/HighlightedPlant.js
@@ -1,4 +1,8 @@
 import React, { Component, Fragment } from "react";
+import Title from "antd/es/typography/Title";
+import PlantPlaceholder from "../../../../assets/plant_placeholder.jpg";
+import * as classes from "./HighlightedPlant.module.scss";
+import { months } from "../../../../Constants";
 
 export default class HighlightedPlant extends Component {
   constructor(props) {
@@ -9,11 +13,26 @@ export default class HighlightedPlant extends Component {
     };
   }
 
+  generateSeasonList() {
+    return this.props.plant?.season?.map((season) => (
+      <li className={`${months[season - 1]?.color}`}>
+        {months[season - 1]?.label} {months[season - 1]?.emoji}
+      </li>
+    ));
+  }
+
   render() {
     return (
       <Fragment>
-        Növény neve:
-        {this.props.plant.name}
+        <aside className={classes.SeasonBox}>
+          <ul>{this.generateSeasonList()}</ul>
+        </aside>
+        <div
+          style={{ backgroundImage: `url(${PlantPlaceholder})` }}
+          className={classes.PlantImage}
+        ></div>
+        <Title className={classes.Title}>{this.props.plant.name}</Title>
+        <p>{this.props.plant.details}</p>
       </Fragment>
     );
   }
