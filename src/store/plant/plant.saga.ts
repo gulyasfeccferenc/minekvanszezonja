572331eaee31fc3e07c8886d496c5947bad9a70a
gulyasfeccferenc <gulyasfeccferenc@gmail.com>
2022-08-31 22:23:25 +0200

    Typing store and saga

--- src/store/plant/plant.saga.ts
+++ src/store/plant/plant.saga.ts
@@ -1,16 +1,15 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'typed-redux-saga';
 import {getCategoriesAndDocuments} from '../../utils/firebase/firebase.utils';
-import {fetchPlantsFailed, fetchPlantsStart, fetchPlantsSuccess} from './plant.action';
+import {fetchPlantsFailed, fetchPlantsSuccess} from './plant.action';
 import {PLANT_ACTION_TYPES} from './plant.types';
 
 
 export function* fetchPlantsAsync() {
     try {
-        // @ts-ignore
-        const plantsArray = yield call(getCategoriesAndDocuments, 'plants');
-        yield put(fetchPlantsSuccess(plantsArray))
-    } catch (error: any) {
-        yield put(fetchPlantsFailed(error))
+        const plantsArray = yield* call(getCategoriesAndDocuments, 'plants');
+        yield* put(fetchPlantsSuccess(plantsArray))
+    } catch (error) {
+        yield* put(fetchPlantsFailed(error as Error))
     }
 }
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,21 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import {compose, createStore, applyMiddleware, Middleware} from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import { rootReducer } from './root-reducer';
-
-
 import { rootSaga } from './root-saga';
 
 export type RootState = ReturnType<typeof rootReducer>;
+
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
+/* Will be handy with implementing redux-persist
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+    whitelist: (keyof RootState)[]
+}*/
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -14,13 +23,13 @@ const middleWares = [
     process.env.NODE_ENV !== 'production'
     && logger,
     sagaMiddleware
-].filter(Boolean);
+].filter((middleware): middleware is Middleware => Boolean(middleware));
 
-// @ts-ignore
-const composeEnhancer: any = (process.env.NODE_ENV !== 'production'  && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+const composeEnhancer = (process.env.NODE_ENV !== 'production'
+        && window
+        && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
     || compose;
 
-// @ts-ignore
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(rootReducer, undefined, composedEnhancers);
